e0563d90530bebb8f27351f1c917d692487f02be
TheUltimateNuke <nuke.public@gmail.com>
2025-09-24 06:00:42 -0400

    Add project files.

---
+++ src/ui/bpm_scale_bump.gd
@@ -0,0 +1,15 @@
+extends Label
+
+@export var rhythm_notifier: RhythmNotifier
+
+func _ready() -> void:
+	rhythm_notifier.beat.connect(_bump)
+
+
+func _process(delta: float) -> void:
+	rotation += 0.007 * delta
+
+
+func _bump(_beat: int):
+	$AnimationPlayer.stop()
+	$AnimationPlayer.play("bump")
