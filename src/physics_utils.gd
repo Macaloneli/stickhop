7dc6ca3cc7533b164c2160286cc90b6797b16ab1
nukep <nuke.public@gmail.com>
2025-09-24 14:19:49 -0400

    Remove some unused scripts

--- src/physics_utils.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-class_name PhysicsUtils
-
-
-static func hookes_law(displacement: float, current_velocity: float, stiffness: float, damping: float):
-	return (stiffness * displacement) - (damping * current_velocity)
